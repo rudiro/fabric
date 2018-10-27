68bd7a91d011e4dd8bb7ea2d604dfda9d0e61742
Röckelein Rudolf <roeckelein@proflow.at>
2018-10-27 09:54:57 +0200

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,44 +1,163 @@
+#https://www.youtube.com/watch?v=bO5jeCYkSFY
+#https://github.com/InflatibleYoshi/fabric-1.1-kafka-multi-orderer
+#https://www.youtube.com/watch?v=gAxK6zYrfxI
+
 provider "aws" {
-  region = "us-east-1"
+  region     = "${var.region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+
+  version = "1.8"
 }
 
 variable "n" {
   default = 2
 }
 
-variable "public_key_path" {
-  description = "Enter the path to the SSH Public Key to add to AWS."
-  default     = "~/.ssh/id_rsa.pub"
+resource "aws_vpc" "fabric-vpc" {
+   cidr_block           = "${var.cidr_block}"
+   #tags {
+     # PROBLEM NOT POSSIBLE WHEN IMPORTING VPC
+    #Name = "${var.environment}_${var.project}_${var.product}"
+    #Name = "get_productive"
+  #}
 }
 
-resource "aws_key_pair" "auth" {
-  key_name   = "default"
-  public_key = "${file(var.public_key_path)}"
+resource "aws_subnet" "fabric-public-subnet" {
+  vpc_id                  = "${aws_vpc.fabric-vpc.id}"
+  cidr_block              = "10.160.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-central-1a"
+  tags {
+    Name = "FABRIC_MULTINODE_PUBLIC_SUBNET"
+  }
+
 }
+resource "aws_eip" "eip" {
+  vpc      = true
+  #depends_on = ["aws_internet_gateway.igw"]
+  tags {
+    Name = "${var.environment}_${var.project}_${var.product}_EIP"
+  }
+
+}
+
+resource "aws_nat_gateway" "natgw_az1" {
+    allocation_id = "${aws_eip.eip.id}"
+    subnet_id = "${aws_subnet.fabric-public-subnet.id}"
+    #depends_on = ["aws_internet_gateway.igw"]
+    tags {
+      Name = "${var.environment}_${var.project}_${var.product}_NATGW_az1"
+    }
+
+}
+
+resource "aws_route_table" "publicroute" {
+  vpc_id = "${aws_vpc.fabric-vpc.id}"
+  tags {
+    Name = "${var.environment}_${var.project}_${var.product}_publicroute"
+  }
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = "${aws_route_table.publicroute.id}"
+  #gateway_id             = "${aws_internet_gateway.igw.id}"
+  gateway_id             = "igw-0fdb5240939b77ebf"
+  
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "internet" {
+  route_table_id         = "${aws_route_table.publicroute.id}"
+  #gateway_id             = "${aws_internet_gateway.igw.id}"
+  gateway_id             = "igw-0fdb5240939b77ebf"
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table" "internroute" {
+  vpc_id = "${aws_vpc.fabric-vpc.id}"
+  tags {
+    Name = "${var.environment}_${var.project}_${var.product}_internroute"
+  }
+
+}
+
+resource "aws_route" "nat" {
+  route_table_id         = "${aws_route_table.internroute.id}"
+  nat_gateway_id         = "${aws_nat_gateway.natgw_az1.id}"
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "route_subnet_3" {
+  subnet_id      = "${aws_subnet.fabric-public-subnet.id}"
+  route_table_id = "${aws_route_table.publicroute.id}"
+}
+
+
+resource "aws_route_table_association" "route_private_subnet_1" {
+  subnet_id      = "${aws_subnet.fabric-subnet.id}"
+  route_table_id = "${aws_route_table.internroute.id}"
+}
+
+
+
+
+
+
+
+
+
+
+resource "aws_subnet" "fabric-subnet" {
+  vpc_id                  = "${aws_vpc.fabric-vpc.id}"
+  cidr_block              = "10.160.2.0/24"
+  map_public_ip_on_launch = false
+  availability_zone       = "eu-central-1a"
+  tags {
+    Name = "FABRIC_MULTINODE_SUBNET"
+  }
+
+}
+
+
+
+#variable "public_key_path" {
+#  description = "Enter the path to the SSH Public Key to add to AWS."
+#  default     = "/Users/rudiro/.ssh/kafka_aws.pub"
+#}
+
+#resource "aws_key_pair" "auth" {
+#  key_name   = "HL_UBUNTU"
+#  #public_key = "${file(var.public_key_path)}"
+#}
 
 resource "aws_efs_file_system" "blockchain-data" {}
 
 resource "aws_efs_mount_target" "multi-machine-HLF11" {
-  ip_address      = "192.168.1.240"
+  ip_address      = "10.160.2.240"
   file_system_id  = "${aws_efs_file_system.blockchain-data.id}"
   subnet_id       = "${aws_subnet.fabric-subnet.id}"
   security_groups = ["${aws_security_group.allow_http.id}"]
   depends_on      = ["aws_efs_file_system.blockchain-data"]
+
 }
 
 resource "aws_instance" "fabric" {
   count                  = 1
-  availability_zone      = "us-east-1a"
-  private_ip             = "192.168.1.5"
-  ami                    = "ami-a07d46df"
+  availability_zone      = "eu-central-1a"
+  private_ip             = "10.160.2.5"
+  ami                    = "ami-062903ba092e9a0b5"
   instance_type          = "t2.large"
-  key_name               = "default"
+  key_name               = "HL_UBUNTU"
   subnet_id              = "${aws_subnet.fabric-subnet.id}"
   vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
 
+  tags {
+    Name = "FABRIC_MULTINODE_PEERS_FABRIC"
+  }
   provisioner "remote-exec" {
     inline = [
-      "sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 192.168.1.240:/ /home/ubuntu/fabric-1.1-kafka-multi-orderer",
+      "sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 10.160.2.240:/ /home/ubuntu/fabric-1.1-kafka-multi-orderer",
       "sudo chown -R ubuntu fabric-1.1-kafka-multi-orderer",
       "source .profile",
       "git clone https://github.com/InflatibleYoshi/fabric-1.1-kafka-multi-orderer",
@@ -59,6 +178,8 @@ resource "aws_instance" "fabric" {
     connection {
       type = "ssh"
       user = "ubuntu"
+      #key_file = "/Users/rudiro/.ssh/HL_UBUNTU"
+      private_key = "${file("/Users/rudiro/.ssh/HL_UBUNTU")}"
     }
   }
 
@@ -67,17 +188,17 @@ resource "aws_instance" "fabric" {
 
 resource "aws_instance" "fabric-peers" {
   count                  = "${var.n - 1}"
-  availability_zone      = "us-east-1a"
-  private_ip             = "192.168.1.${count.index + 6}"
-  ami                    = "ami-a07d46df"
+  availability_zone      = "eu-central-1a"
+  private_ip             = "10.160.2.${count.index + 6}"
+  ami                    = "ami-062903ba092e9a0b5"
   instance_type          = "t2.large"
-  key_name               = "default"
+  key_name               = "HL_UBUNTU"
   subnet_id              = "${aws_subnet.fabric-subnet.id}"
   vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
 
   provisioner "remote-exec" {
     inline = [
-      "sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 192.168.1.240:/ /home/ubuntu/fabric-1.1-kafka-multi-orderer",
+      "sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 10.160.2.240:/ /home/ubuntu/fabric-1.1-kafka-multi-orderer",
       "sudo chown -R ubuntu fabric-1.1-kafka-multi-orderer",
     ]
 
@@ -92,33 +213,15 @@ resource "aws_instance" "fabric-peers" {
     connection {
       type = "ssh"
       user = "ubuntu"
+      private_key = "${file("/Users/rudiro/.ssh/HL_UBUNTU")}"
     }
   }
 
-  depends_on = ["aws_instance.fabric"]
-}
-
-resource "aws_vpc" "fabric-vpc" {
-  cidr_block           = "192.168.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-}
-
-resource "aws_internet_gateway" "default" {
-  vpc_id = "${aws_vpc.fabric-vpc.id}"
-}
-
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.fabric-vpc.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.default.id}"
-}
-
-resource "aws_subnet" "fabric-subnet" {
-  vpc_id                  = "${aws_vpc.fabric-vpc.id}"
-  cidr_block              = "192.168.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  #depends_on = ["aws_instance.fabric"]
+  depends_on = ["aws_efs_mount_target.multi-machine-HLF11"]
+  tags {
+    Name = "FABRIC_MULTINODE_PEERS"
+  }
 }
 
 resource "aws_security_group" "allow_http" {
@@ -130,7 +233,7 @@ resource "aws_security_group" "allow_http" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["192.168.1.0/24"]
+    cidr_blocks = ["10.160.2.0/24"]
   }
 
   ingress {
