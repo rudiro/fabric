2471fd4b8ff03f1e3e41a1eb4e388545a792361d
Röckelein Rudolf <roeckelein@proflow.at>
2018-10-28 09:56:24 +0100

    1.3.0

--- myCommandsPeers.sh
+++ myCommandsPeers.sh
@@ -20,9 +20,9 @@ composer-playground
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
-9a71084dcac5        hyperledger/fabric-orderer:x86_64-1.1.0                                                                                                      "orderer"                About an hour ago   Up About an hour    0.0.0.0:7050->7050/tcp                             orderer1.example.com
-18e29827a615        hyperledger/fabric-peer:x86_64-1.1.0                                                                                                         "peer node start"        About an hour ago   Up About an hour    0.0.0.0:10051->7051/tcp, 0.0.0.0:10053->7053/tcp   peer3.org1.example.com
-dc51fc223456        hyperledger/fabric-peer:x86_64-1.1.0                                                                                                         "peer node start"        About an hour ago   Up About an hour    0.0.0.0:9051->7051/tcp, 0.0.0.0:9053->7053/tcp     peer2.org1.example.com
+9a71084dcac5        hyperledger/fabric-orderer:x86_64-1.3.0                                                                                                      "orderer"                About an hour ago   Up About an hour    0.0.0.0:7050->7050/tcp                             orderer1.example.com
+18e29827a615        hyperledger/fabric-peer:x86_64-1.3.0                                                                                                         "peer node start"        About an hour ago   Up About an hour    0.0.0.0:10051->7051/tcp, 0.0.0.0:10053->7053/tcp   peer3.org1.example.com
+dc51fc223456        hyperledger/fabric-peer:x86_64-1.3.0                                                                                                         "peer node start"        About an hour ago   Up About an hour    0.0.0.0:9051->7051/tcp, 0.0.0.0:9053->7053/tcp     peer2.org1.example.com
 a05201f41f06        hyperledger/fabric-couchdb:x86_64-0.4.6                                                                                                      "tini -- /docker-ent…"   About an hour ago   Up About an hour    4369/tcp, 9100/tcp, 0.0.0.0:8984->5984/tcp         couchdb3
 91dc367d6239        hyperledger/fabric-couchdb:x86_64-0.4.6                                                                                                      "tini -- /docker-ent…"   About an hour ago   Up About an hour    4369/tcp, 9100/tcp, 0.0.0.0:7984->5984/tcp         couchdb2
 
@@ -31,9 +31,9 @@ a05201f41f06        hyperledger/fabric-couchdb:x86_64-0.4.6                     
 
 ubuntu@ip-192-168-1-6:~/fabric-1.1-kafka-multi-orderer$ docker ps
 CONTAINER ID        IMAGE                                     COMMAND                  CREATED             STATUS              PORTS                                              NAMES
-9a71084dcac5        hyperledger/fabric-orderer:x86_64-1.1.0   "orderer"                10 minutes ago      Up 10 minutes       0.0.0.0:7050->7050/tcp                             orderer1.example.com
-18e29827a615        hyperledger/fabric-peer:x86_64-1.1.0      "peer node start"        10 minutes ago      Up 10 minutes       0.0.0.0:10051->7051/tcp, 0.0.0.0:10053->7053/tcp   peer3.org1.example.com
-dc51fc223456        hyperledger/fabric-peer:x86_64-1.1.0      "peer node start"        10 minutes ago      Up 10 minutes       0.0.0.0:9051->7051/tcp, 0.0.0.0:9053->7053/tcp     peer2.org1.example.com
+9a71084dcac5        hyperledger/fabric-orderer:x86_64-1.3.0   "orderer"                10 minutes ago      Up 10 minutes       0.0.0.0:7050->7050/tcp                             orderer1.example.com
+18e29827a615        hyperledger/fabric-peer:x86_64-1.3.0      "peer node start"        10 minutes ago      Up 10 minutes       0.0.0.0:10051->7051/tcp, 0.0.0.0:10053->7053/tcp   peer3.org1.example.com
+dc51fc223456        hyperledger/fabric-peer:x86_64-1.3.0      "peer node start"        10 minutes ago      Up 10 minutes       0.0.0.0:9051->7051/tcp, 0.0.0.0:9053->7053/tcp     peer2.org1.example.com
 4e01c5893903        hyperledger/fabric-kafka                  "/docker-entrypoint.…"   10 minutes ago      Up 10 minutes       9093/tcp, 0.0.0.0:10092->9092/tcp                  kafka5
 cc44fcdd319f        hyperledger/fabric-kafka                  "/docker-entrypoint.…"   10 minutes ago      Up 10 minutes       9093/tcp, 0.0.0.0:11092->9092/tcp                  kafka6
 552f3ed36d1a        hyperledger/fabric-kafka                  "/docker-entrypoint.…"   10 minutes ago      Up 10 minutes       9093/tcp, 0.0.0.0:12092->9092/tcp                  kafka7
